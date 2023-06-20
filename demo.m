6dc99b4d7fb47859391fe1dd64f19f6e45139269
Vojtech Cvrcek <cvrcekv@gmail.com>
2023-06-21 00:30:36 +0200

    removing old demo

--- demo.m
+++ demo.m
@@ -1,36 +1,9 @@
 %% load image
-n = 501;
-if 0
-    rs0 = [10, 10, 10];
-    rs = {rs0, rs0, rs0};
-    lens0 = [100, 50, 25];
-    lens = {lens0, lens0, lens0};
-    [ims, streaks, streaks_mid_gt, streaks_idx_gt, phis_gt, star_locs] = ...
-                                         artificial_image(...
-                                            'num_of_streaks', 3,...
-                                            'num_of_stars', 0,...
-                                            'streak_amplitudes', [1.5, 2.3, 2],...
-                                            'stars_amplitudes', [],...
-                                            'rs', rs,...
-                                            'lens', lens,...
-                                            'mn', [n, n]...
-                                            );
-    im = ims{2};
-    save('im.mat', 'im');
-elseif 1
-    [ims, streaks, streaks_mid_gt, streaks_idx_gt, phis_gt, star_locs] = ...
-                                     artificial_image(...
-                                     'mn', [n, n],...
-                                     'streak_amplitudes', [1.5]);
-    im = ims{2};
-    phi = phis_gt{1}{1} + 90;
-    save('im.mat', 'im', 'phi', 'n');
-else
-    data = load('im.mat');
-    im = data.im;
-    phi = data.phi;
-    n = data.n;
-end
+data = load('im.mat');
+im = data.im;
+phi = data.phi;
+n = data.n;
+
     
 figure(1);
 imagesc(im);
